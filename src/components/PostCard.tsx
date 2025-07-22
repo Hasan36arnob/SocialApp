dc56a38e012e0ac74e524441c00308e1f910a7aa
Hasan <arnobhasanice@gmail.com>
2025-07-22 12:06:13 +0600

    first commit

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PostCard({ post, dbUserId }: { post: any, dbUserId: any }) {
+  return (
+    <div>PostCard</div>
+  )
+}
+
+export default PostCard
---
+++ src/lib/prisma.ts
@@ -0,0 +1,14 @@
+
+import { PrismaClient } from '../generated/prisma'; // relative path from src/lib/prisma.ts
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
